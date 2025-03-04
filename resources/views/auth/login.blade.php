fc4267fcdc70d2dc762459fd50ddc72b5d12ba0c
Hoang PH <hoangph@vietis.com.vn>
2025-03-04 16:19:10 +0700

    call layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,10 @@
+@extends('layouts.app')
 <!DOCTYPE html>
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng Nhập</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body class="d-flex align-items-center justify-content-center">
     <div class="card p-4">
